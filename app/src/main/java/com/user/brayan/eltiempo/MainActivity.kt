a5f07ac4c7b9c9f086872f7c9c399e19a897aedb
brayanSV <villarragabrayan@hotmail.com>
2021-09-20 13:39:03 -0500

    Primer Commit

---
+++ app/src/main/java/com/user/brayan/eltiempo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.user.brayan.eltiempo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
